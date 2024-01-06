f99ffd567d8e0a4e87fcbe1c11fa3fd76bd938c1
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-01-06 21:57:04 +0500

    t1.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="relative sm:text-xs">
+    <div className="relative sm:ml-24">
       <div className="fixed w-full z-50 top-0 left-0 bg-white text-black text-xl py-4 flex items-center justify-center font-NavFont">
         <div className="items-center flex">
           <Link to={"/"} className="link">
