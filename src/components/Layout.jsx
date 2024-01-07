a4490c7831eb3e164e3e578ac741eb7ea5bffe79
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-01-08 01:29:17 +0500

    t1.6.3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
-import SecondFooter from './SecongFooter'
+import SecondFooter from './SecondFooter'
 
 export default function Layout({children}) {
   return (
