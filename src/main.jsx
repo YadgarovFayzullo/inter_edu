1d71c396c6266e35ffc34f5036b54e64195b1b75
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-01-12 20:50:52 +0500

    test

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./index.css";
-import { inject } from '@vercel/analytics';
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-inject();
