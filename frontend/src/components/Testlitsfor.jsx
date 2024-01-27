7875c8192abf6d66966ad04b25b8bfcc0055711b
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-01-27 20:41:01 +0500

    pfd

---
+++ frontend/src/components/Testlitsfor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function testlitsfor() {
+  return (
+    <div className="mb-[30%] flex text-center mt-56">
+      <h1>HAHHAHAHA URURURUURURURURURUR</h1>
+      dduududfdufhudhfdfdfefdffdfdfdfdf
+      <strong>ffdfdfdf                                                    d</strong>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Testlist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Testlistfor from '../components/Testlitsfor'
+
+export default function testList() {
+  return (
+  <Layout>
+    <Testlistfor />
+  </Layout>
+  )
+}
