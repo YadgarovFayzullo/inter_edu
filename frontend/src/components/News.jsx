702d2e820b94959b826c4d0aa3b890a621e1b5b7
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-05-15 17:30:26 +0500

    second fix

--- frontend/src/components/News.jsx
+++ frontend/src/components/News.jsx
@@ -2,26 +2,39 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FaRegEye } from "react-icons/fa";
 import { CiCalendar } from "react-icons/ci";
-import axios from "axios";
-
 export default function News() {
   const { t } = useTranslation();
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 2;
-  const [newsItems, setNewsItems] = useState([]);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/news");
-        setNewsItems(response.data);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    };
-
-    fetchNews();
-  }, []);
+  const newsItems = [
+    {
+      title: "Прием на новый выпуск",
+      description:
+        "Продолжается прием материалов для нового выпуска журнала. Мы оцениваем оригинальность, актуальность и научную значимость предлагаемых материалов. Благодарим вас за вашу заинтересованность и участие!",
+      imageUrl: "/3-Meraki.png",
+      link: "https://t.me/IEandGS",
+    },
+    {
+      title: "Конкурс на лучшую статью",
+      description:
+        "Конкурс для статей объявленных 2024 году во втором выпуске. Правила участия и призы скоро будут объявлены. Следите за нами.",
+      imageUrl: "/news-2024.png",
+      link: "https://t.me/IEandGS",
+    },
+    {
+      title: "Ознокомтесь правилами",
+      description:
+        "Пожалуйста, убедитесь, что ваша статья соответствует требованиям для подачи материалов. Благодарим вас за вашу заинтересованность и участие!",
+      imageUrl: "/news-new2.jpeg",
+      link: "https://t.me/IEandGS",
+    },
+    {
+      title: "Статистика",
+      description:
+        "За месяц наш сайт был просмотрен более 5 тысяч раз со всего мира. Следите за нами чтобы не пропускать новости",
+      imageUrl: "/growing.png",
+    },
+  ];
 
   const handleClick = (page) => {
     setCurrentPage(page);
@@ -36,7 +49,7 @@ export default function News() {
       <div className="text-center">
         <h1 className="text-[40px]">{t("news")}</h1>
       </div>
-      <div className="flex justify-around">
+      <body className="flex justify-around">
         <div className="flex flex-row gap-10 md:gap-28 md:justify-around flex-wrap mt-12 max-w-[70%] md:max-w-full">
           {currentItems.map((item, index) => (
             <a
@@ -46,15 +59,15 @@ export default function News() {
             >
               <img
                 className="object-cover w-full rounded-t-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                src={item.imagePath}
+                src={item.imageUrl}
                 alt="image"
               />
               <div className="flex flex-col justify-between p-2 leading-normal">
-                <h2   className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
+                <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
                   {item.title}
                 </h2>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {item.text}
+                  {item.description}
                 </p>
                 {/* <div className="flex gap-12">
                   <p>
@@ -68,7 +81,7 @@ export default function News() {
             </a>
           ))}
         </div>
-      </div>
+      </body>
       <div className="flex justify-center mt-5">
         {Array.from({ length: Math.ceil(newsItems.length / itemsPerPage) }).map(
           (_, index) => (
