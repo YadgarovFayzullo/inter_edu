e1719077ab1af92c87d0bcbb955d09d52d9aa40a
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-05-15 22:29:18 +0500

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
-  build: {
-    rollupOptions: {
-      external: ["axios"],
-    },
-  },
+  build: {},
   define: {
     "process.env": {},
   },
